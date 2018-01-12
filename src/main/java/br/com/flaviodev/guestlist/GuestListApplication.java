313a5e230ddf954aea957f54ac61c1fc94505a13
flavio <fdsdev@gmail.com>
2018-01-12 14:02:45 -0200

    creating project

---
+++ src/main/java/br/com/flaviodev/guestlist/GuestListApplication.java
@@ -0,0 +1,12 @@
+package br.com.flaviodev.guestlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestListApplication.class, args);
+	}
+}
