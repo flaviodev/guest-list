81cb1fbb033811475ae49b7bea4689755e899120
flavio <fdsdev@gmail.com>
2018-01-22 14:21:20 -0200

    configuring properties

--- src/main/java/br/com/flaviodev/guestlist/controller/GuestController.java
+++ src/main/java/br/com/flaviodev/guestlist/controller/GuestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import br.com.flaviodev.EmailSender.services.EmailService;
+import br.com.flaviodev.emailsender.services.EmailService;
 import br.com.flaviodev.guestlist.model.Guest;
 import br.com.flaviodev.guestlist.services.GuestService;
 
